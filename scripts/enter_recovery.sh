9339f268b0d17de13786951e6d8910f2f9134cc7
sen0rxol0 <sen0rxol0@users.noreply.github.com>
2023-10-12 16:16:41 +0200

    Update enter_recovery.sh

--- scripts/enter_recovery.sh
+++ scripts/enter_recovery.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
-# Somehow device has to be spammed !
-# spamming `ideviceinfo` makes the restart process
-# with device connected it will enter recovery mode
-# device does not enter recovery mode with `ideviceenterrecovery`
 idevicepair pair
 idevicediagnostics -u $(ideviceinfo -k UniqueDeviceID) restart
 ideviceinfo
